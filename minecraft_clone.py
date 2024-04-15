ea5949cbadcfea14c1d2dd4ed5a2dbcf8fcf6c4e
PraneethJain <praneethjain005@gmail.com>
2024-04-15 17:41:32 +0530

    feat: terrain generation

--- minecraft_clone.py
+++ minecraft_clone.py
@@ -1,24 +1,27 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from itertools import product
+from height_map import HillGrid
 
 
 app = Ursina()
 WALK_SPEED = 5
 RUN_SPEED = 7
+GRID_SIZE = 32
 
 grass_texture = load_texture("assets/grass.png")
+dirt_texture = load_texture("assets/dirt.png")
 punch_sound = Audio("assets/punch.wav", loop = False, autoplay = False)
 
 
 class Voxel(Button):
-    def __init__(self, position=(0, 0, 0)):
+    def __init__(self, position=(0, 0, 0), texture=dirt_texture):
         super().__init__(
             parent=scene,
             position=position,
             model="assets/block",
             origin_y=0.5,
-            texture=grass_texture,
+            texture=texture,
             color=color.color(0, 0, random.uniform(0.9, 1)),
             scale = 0.5
         )
@@ -51,12 +54,16 @@ def input(key):
 
 
 def generate_floor() -> None:
-    for x, z in product(range(16), repeat=2):
-        Voxel(position=(x, 0, z))
+    heights = HillGrid(SIZE=GRID_SIZE, ITER=20)
+    for x, z in product(range(GRID_SIZE), repeat=2):
+        for y in range(0, heights[x][z]):
+            Voxel(position=(x, y, z))
+        Voxel(position=(x, heights[x][z], z), texture=grass_texture)
 
 
 if __name__ == "__main__":
     generate_floor()
     player = FirstPersonController()
+    player.mouse_sensitivity = Vec2(100, 100)
     player.speed = WALK_SPEED
     app.run()
